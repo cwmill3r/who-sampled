df914d0b4dd3f29706b0c02a1ded4b1ef1449bfb
cwmill3r <chris.wayne.miller@gmail.com>
2019-02-10 15:21:48 -0700

    we can now fetch our playlist of videos

--- youtube/youtube.js
+++ youtube/youtube.js
@@ -1,35 +1,37 @@
 var KEY = 'AIzaSyD11iA-eBNZjIhZ5FtVJhqTkZEgph6zrXY';
+var PLAYLIST = 'PLM5JVEgIGu5pdPh59wO0Q8QaNcCwVtm_M';
 
 // include axios from CDN -- see youtube.html
 
 var youtube = axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
   params: {
-    part: 'snippet',
-    maxresults: 5,
+    part: 'snippet, contentDetails',
+    maxresults: 10,
     key: KEY
   }
 });
 
-async function onTermSubmit(e) {
+async function handleClick(e) {
   e.preventDefault();
-  var searchTerm = document.querySelector('#textBox').value;
-  console.log(searchTerm);
-  var response = await youtube.get('/search', {
+
+  var response = await youtube.get('/playlistItems', {
     params: {
-      q: searchTerm
+      playlistId: PLAYLIST
     }
   });
   console.log(response.data.items);
   var videoList = response.data.items;
 
   videoList.map(function(video) {
-    console.log(video.id.videoId);
+    console.log(video.contentDetails.videoId);
     console.log(video.snippet.title);
     document.querySelector('body').innerHTML += `
       <div>
         <div>
-          <iframe src="https://www.youtube.com/embed/${video.id.videoId}" />
+          <iframe src="https://www.youtube.com/embed/${
+            video.contentDetails.videoId
+          }" />
         </div>
         <div>
           ${video.snippet.title}
@@ -39,4 +41,44 @@ async function onTermSubmit(e) {
   });
 }
 
-document.querySelector('#searchForm').addEventListener('submit', onTermSubmit);
+document.querySelector('#videosButton').addEventListener('click', handleClick);
+
+// OLD CODE FOR SEARCH
+// var youtube = axios.create({
+//   baseURL: 'https://www.googleapis.com/youtube/v3',
+//   params: {
+//     part: 'snippet',
+//     maxresults: 5,
+//     key: KEY
+//   }
+// });
+
+// async function onTermSubmit(e) {
+//   e.preventDefault();
+//   var searchTerm = document.querySelector('#textBox').value;
+//   console.log(searchTerm);
+//   var response = await youtube.get('/search', {
+//     params: {
+//       q: searchTerm
+//     }
+//   });
+//   console.log(response.data.items);
+//   var videoList = response.data.items;
+
+//   videoList.map(function (video) {
+//     console.log(video.id.videoId);
+//     console.log(video.snippet.title);
+//     document.querySelector('body').innerHTML += `
+//       <div>
+//         <div>
+//           <iframe src="https://www.youtube.com/embed/${video.id.videoId}" />
+//         </div>
+//         <div>
+//           ${video.snippet.title}
+//         </div>
+//       </div>
+//     `;
+//   });
+// }
+
+// document.querySelector('#searchForm').addEventListener('submit', onTermSubmit);
