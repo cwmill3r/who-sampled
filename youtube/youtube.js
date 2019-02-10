c251b367caf9652f09e543e7638ef38df43cf5a2
cwmill3r <chris.wayne.miller@gmail.com>
2019-02-10 14:34:59 -0700

    made youtube work with search

--- youtube/youtube.js
+++ youtube/youtube.js
@@ -2,7 +2,7 @@ var KEY = 'AIzaSyD11iA-eBNZjIhZ5FtVJhqTkZEgph6zrXY';
 
 // include axios from CDN -- see youtube.html
 
-axios.create({
+var youtube = axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
   params: {
     part: 'snippet',
@@ -11,8 +11,32 @@ axios.create({
   }
 });
 
-function onTermSubmit() {
-  console.log('submitted');
+async function onTermSubmit(e) {
+  e.preventDefault();
+  var searchTerm = document.querySelector('#textBox').value;
+  console.log(searchTerm);
+  var response = await youtube.get('/search', {
+    params: {
+      q: searchTerm
+    }
+  });
+  console.log(response.data.items);
+  var videoList = response.data.items;
+
+  videoList.map(function(video) {
+    console.log(video.id.videoId);
+    console.log(video.snippet.title);
+    document.querySelector('body').innerHTML += `
+      <div>
+        <div>
+          <iframe src="https://www.youtube.com/embed/${video.id.videoId}" />
+        </div>
+        <div>
+          ${video.snippet.title}
+        </div>
+      </div>
+    `;
+  });
 }
 
 document.querySelector('#searchForm').addEventListener('submit', onTermSubmit);
