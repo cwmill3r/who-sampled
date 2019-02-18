8b51fccdd91a900aea26ed59649ee457ccd5215f
aguise1 <aguise@asu.edu>
2019-02-17 19:34:11 -0700

    Second commit with added quiz HTML/css

---
+++ accountmanager/who-sampled-iss53/account-manager-API/accountmanager/Account.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace accountmanager
+{
+    public class Account
+    {
+        // just a container for info related to account
+        public int id;
+        public string userId;
+        public string password;
+        public string firstName;
+        public string lastName;
+        public string email;
+    }
+}
